4c4efecefbad6b99e678b200b346b6edcae1c911
thecollegeproject <38028893+thecollegeproject@users.noreply.github.com>
2018-05-18 02:53:55 +0530

    Update one.js

--- TD/one.js
+++ TD/one.js
@@ -1,9 +1,20 @@
 var leftBar=document.getElementById('block');
+var wrap;
 var leftBar1=document.getElementById('copy1').innerHTML;
+var myListInnerHtml;
+var list02=document.getElementById('copy3').innerHTML;
+var list01= document.getElementById('copy2').innerHTML;
+
+var i=0,j=0,k=0;
+var myList=[];
+var myList1=[];
+var myList2=[];
+
 
 var count=0;
 var dcount=0;
 var down=0;
+
 
 function menu()
 
@@ -42,12 +53,12 @@ function menu1()
     leftBar.innerHTML=leftBar1;
     leftBar.style.width="75%";
     leftBar.style.padding="10px";
-    leftBar.style.border="2px solid black";
     leftBar.style.backgroundColor="white";
     leftBar.style.marginLeft="-12px";
              leftBar.style.boxShadow="2px 2px 14px 1px grey";
             count++;
         }
+   
 }
 
 
@@ -86,12 +97,31 @@ var inputTD=item.value;
             new1.innerHTML=inputTD+"<button id='cross' onclick=rm(this) style='float:right'>x</botton>";
             olList.appendChild(new1);
             item.value='';
+            if(olList.parentElement.id=="myList")
+                {
+                    myList[i]=inputTD;
+                    i++;
+                }
+            else if(olList.parentElement.id=="list01")
+                {
+                    myList1[j]=inputTD;
+                    j++;
+                }
+            else if(olList.parentElement.id=="list02")
+                {
+                    myList2[k]=inputTD;
+                    k++;
+                }
+            else{
+                
+            }
         }
   
 }
 
 function rm(e)
 {
+    var id=e.parentElement.parentElement.parentElement.id;
     var co =e.parentElement;
     e.remove();
    var po=co.textContent;
@@ -100,7 +130,61 @@ function rm(e)
     var new1 = document.createElement('li');
             new1.innerHTML=po+"<button id='restore' onclick=restore(this) style='position:absolute;left:80vw;top:'"+down+"'>R</botton>";
             ol.appendChild(new1);
+    
      co.remove();
+    
+     if(id=="myList")
+                {
+                   var a= myList.length;
+    
+ 
+    for(var x=0;x<a;x++)
+        {
+           if(myList[x]==po)
+               {
+                  myList.splice(x,1);
+                   i--;
+                   break;
+               }
+        }
+                    
+                }
+            else if(id=="list01")
+                {
+                    var a= myList1.length;
+    
+ 
+    for(var x=0;x<a;x++)
+        {
+           if(myList1[x]==po)
+               {
+                   myList1.splice(x,1);
+                   j--;
+                   break;
+               }
+        }
+                 
+                }
+            else if(id=="list02")
+                {
+                     var a= myList2.length;
+    
+ 
+    for(var x=0;x<a;x++)
+        {
+           if(myList2[x]==po)
+               {
+                   myList2.splice(x,1);
+                   k--;
+                   break;
+               }
+        }
+                 
+                }
+            else{
+                
+            }
+    
     
 }
 
@@ -138,61 +222,150 @@ function restore(e)
             var new1 = document.createElement('li');
             new1.innerHTML=inputTD+"<button id='cross' onclick=rm(this) style='float:right'>x</botton>";
             olList.appendChild(new1);
+     if(olList.parentElement.id=="myList")
+                {
+                    myList[i]=inputTD;
+                    i++;
+                }
+            else if(olList.parentElement.id=="list01")
+                {
+                    myList1[j]=inputTD;
+                    j++;
+                }
+            else if(olList.parentElement.id=="list02")
+                {
+                    myList2[k]=inputTD;
+                    k++;
+                }
+            else{
+                
+            }
             obj.remove();
     down=down-25;
   
 }
 
+function list1()
+{
+
+    wrap=document.getElementById('wrap');
+    var chId=wrap.childNodes[3].id;
+    
+       if(chId=="myList")
+        {
+            myListInnerHtml=wrap.innerHTML;
+        }
+    else if(chId=="list01")
+        {
+            list01=wrap.innerHTML;
+        }
+    else if(chId=="list02")
+        {
+            list02=wrap.innerHTML;
+        }
+    else
+        {
+            
+        }
+    wrap.innerHTML=list01;
+    allUnset();
+}
+
+function myLis()
+{
+    wrap=document.getElementById('wrap');
+     var chId=wrap.childNodes[3].id;
+    if(chId=="myList")
+        {
+            myListInnerHtml=wrap.innerHTML;
+        }
+    else if(chId=="list01")
+        {
+            list01=wrap.innerHTML;
+        }
+    else if(chId=="list02")
+        {
+            list02=wrap.innerHTML;
+        }
+    else
+        {
+            
+        }
+
+    wrap.innerHTML=myListInnerHtml;
+    allUnset();
+}
 
 
+function list2()
+{
+    wrap=document.getElementById('wrap');
+     var chId=wrap.childNodes[3].id;
+    if(chId=="myList")
+        {
+            myListInnerHtml=wrap.innerHTML;
+        }
+    else if(chId=="list01")
+        {
+            list01=wrap.innerHTML;
+        }
+    else if(chId=="list02")
+        {
+            list02=wrap.innerHTML;
+        }
+    else
+        {
+            
+        }
 
+    wrap.innerHTML=list02;
+    allUnset();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#block
-//{
-//    width:11px;
-//    position:fixed;
-//   margin-left: -6px;
-//    margin-bottom: -102px;
-//    top: 0;
-//    margin-top:0px;
-//   
-//    height: 100vh;
-//    transition-duration: .4s;
-//    z-index: 2;
+function displayArray()
+{
+    var a= myList.length;
+    
+ 
+    for(var x=0;x<a;x++)
+        {
+            document.getElementById('array').innerHTML=document.getElementById('array').innerHTML+"*"+myList[x];
+        }
+    var b= myList1.length;
+    
+     for(var y=0;y<b;y++)
+        {
+            document.getElementById('array').innerHTML=document.getElementById('array').innerHTML+" "+myList1[y];
+        }
+     var c= myList2.length;
+     for(var z=0;z<c;z++)
+        {
+            document.getElementById('array').innerHTML=document.getElementById('array').innerHTML+" "+myList2[z];
+        }
+   // document.getElementById('array').innerHTML="hh";
+    
+}
+//
+//window.onbeforeunload = function() {
 //    
-//    
+//        return "Dus!";
 //}
 
-//#block:hover
-//{
-//    padding: 10px;
-//    border: 2px solid black;
-//height:100vh;
-//    width: 75%;
-//    
-//    
-//    margin-top: -2px;
-//    background-color: red;
-//    transition-duration: .4s;
-//    opacity: 0.4;
-///*display: none;*/
-//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
